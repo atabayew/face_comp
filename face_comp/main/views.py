fc18d12c3e8ead22d822eea029b8febc23981da2
Cyber Security 202206 <202206@tmhi.edu>
2024-03-08 11:43:28 +0500

    tazeleme deding ucin

--- face_comp/main/views.py
+++ face_comp/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def main(request):
-    return render(request,'index.html')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
